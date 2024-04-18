4dc59fc3005f323118948d08556e0b6696feec20
Riiturii <riiturii@gmail.com>
2024-04-18 17:07:05 +0200

    added JSON

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,16 @@
+import { logementsList } from '../../datas/Logements.js'
+
 function Home() {
-  return (
-      <div>
-          <h1> Page d'accueil 🏠</h1>
-      </div>
-  )
+    return (
+        <div>
+            <h1> KASA 🏠</h1>
+            <ul>
+                {logementsList.map((logement) => (
+                    <li key={logement.id}>{logement.title}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default Home
