64684d42d38394deda3f8c1fa3ecf37b76a5536a
Riiturii <riiturii@gmail.com>
2024-04-22 12:08:21 +0200

    Card styles test

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,53 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import colors from '../../styles/colors'
 import '../../styles/Card.css'
+
+const CardTitle = styled.span`
+    color: black;
+    font-size: 22px;
+    font-weight: normal;
+    align-self: center;
+`
+
+const CardImage = styled.img`
+    height: 150px;
+    width: 150px;
+    align-self: center;
+    border-radius: 50%;
+`
+
+const CardWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 15px;
+    background-color: ${colors.backgroundLight};
+    border-radius: 30px;
+    width: 300px;
+    height: 300px;
+    transition: 200ms;
+    &:hover {
+        cursor: pointer;
+        box-shadow: 2px 2px 10px #e2e3e9;
+    }
+`
 
 export default function Card({id, title, cover}) {
 
 	return (
-		<Link to={`/pages/hosting/${id}`} className="gallery_card">
-			<img src={cover} alt={"Kasa -" + title} />
-			<h3>{title}</h3>	
-		</Link>
+		<CardWrapper>
+			<Link to={`/pages/hosting/${id}`} className="gallery_card">
+				<CardImage src={cover} alt={"Kasa -" + title} />
+				<CardTitle>{title}</CardTitle>
+			</Link>
+		</CardWrapper>
 	)
+}
+
+Card.propTypes = {
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
 }
--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,20 +1,33 @@
 import { logementsList } from '../../datas/Logements.js'
+import styled from 'styled-components'
+import colors from '../../styles/colors'
 import Card from '../../components/Card'
+
+const CardsContainer = styled.div`
+    display: grid;
+    gap: 24px;
+    grid-template-rows: 350px 350px;
+    grid-template-columns: repeat(3, 1fr);
+    align-items: center;
+    justify-items: center;
+`
 
 export default function Gallery() {
 
     return (
         <main className='home_gallery'>
-            {logementsList.map(logement => {
-                return (
-                    <Card
-                        key={logement.id}
-                        id={logement.id}
-                        title={logement.title}
-                        cover={logement.cover}
-                    />
-                )
-            })}
+            <CardsContainer>
+                {logementsList.map(logement => {
+                    return (
+                        <Card
+                            key={logement.id}
+                            id={logement.id}
+                            title={logement.title}
+                            cover={logement.cover}
+                        />
+                    )
+                })}
+            </CardsContainer>
         </main>
     )
 }
