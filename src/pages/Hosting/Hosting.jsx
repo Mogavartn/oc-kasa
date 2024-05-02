479a93e1129745f6be51886bef2490f9a65b87c7
Riiturii <riiturii@gmail.com>
2024-05-02 17:31:42 +0200

    début collapse hostings

--- src/pages/Hosting/Hosting.jsx
+++ src/pages/Hosting/Hosting.jsx
@@ -1,9 +1,25 @@
-function Hosting() {
+import './Hosting.scss'
+import hostings from '../../datas/Logements.json'
+import Collapse from '../../components/Collapse/Collapse'
+
+
+export default function Hosting() {
     return (
         <body>
             <h1>Logement</h1>
+            <div className="about__body--collapses">
+                {hostings.map(({ id, equipments }) => {
+                    return (
+                        <div key={id} className="about__body--collapse">
+                            <Collapse
+                                style={{ margin: '30px 0' }}
+                                title={"Equipements"}
+                                content={equipments}
+                            />
+                        </div>
+                    )
+                })}
+            </div>
         </body>
     )
 }
-
-export default Hosting
