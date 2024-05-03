34b5c34812b684bca8b68d8343ccaa86a3190d1d
Riiturii <riiturii@gmail.com>
2024-05-03 18:12:56 +0200

    Hosting infos added OK Need scss + add Rating

--- src/pages/Hosting/Hosting.jsx
+++ src/pages/Hosting/Hosting.jsx
@@ -1,47 +1,61 @@
 import './Hosting.scss'
 import React, { useEffect, useState } from 'react'
-import { useParams } from "react-router-dom"
+import { useParams } from 'react-router-dom'
 import hostings from '../../datas/Logements.json'
 import Collapse from '../../components/Collapse/Collapse'
 import Carousel from '../../components/Carousel/Carousel'
 import Error from '../Error/Error'
 
 export default function Hosting() {
-    const { id } = useParams();
-    const selectedHosting = hostings.find(data => data.id === id);
+    const { id } = useParams()
+    const selectedHosting = hostings.find((data) => data.id === id)
 
-    const [imageSlider, setImageSlider] = useState([]);
+    const [imageSlider, setImageSlider] = useState([])
 
     useEffect(() => {
         if (selectedHosting) {
-            setImageSlider(selectedHosting.pictures);
+            setImageSlider(selectedHosting.pictures)
         }
-    }, [selectedHosting]);
+    }, [selectedHosting])
 
     if (!selectedHosting) {
-        return <Error />;
+        return <Error />
     }
 
-    const description = selectedHosting.description;
-    const equipments = selectedHosting.equipments;
+    const title = selectedHosting.title
+    const location = selectedHosting.location
+    const equipments = selectedHosting.equipments
+    const description = selectedHosting.description
+    const hostName = selectedHosting.host.name
+    const hostPhoto = selectedHosting.host.picture
+
+    const tags = selectedHosting.tags.map((item, index) => (
+        <p key={index}>{item}</p>
+    ))
 
     return (
         <div key={selectedHosting.id} className="selectedHosting">
-                <Carousel
-                    images={imageSlider}
+            <Carousel images={imageSlider} />
+
+            <h2 className="selectedHosting__title">{title}</h2>
+            <p className="selectedHosting__location">{location}</p>
+            <div className="selectedHosting__tags">{tags}</div>
+
+            <div className='selectedHosting__host'>
+                <p className='selectedHosting__host--name'>{hostName}</p>
+                <img 
+                    className='selectedHosting__host--photo'
+                    src={hostPhoto}
+                    alt="photographie de l'hôte du logement"
                 />
-            <div className='selectedHosting__collapses'>
+            </div>
+
+            <div className="selectedHosting__collapses">
                 <div className="selectedHosting__collapses--item">
-                    <Collapse
-                        title={"Equipements"}
-                        content={equipments}
-                />
+                    <Collapse title={'Equipements'} content={equipments} />
                 </div>
                 <div className="selectedHosting__collapses--item">
-                    <Collapse
-                        title={"Description"}
-                        content={description}
-                />
+                    <Collapse title={'Description'} content={description} />
                 </div>
             </div>
         </div>
