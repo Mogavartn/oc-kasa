86dce7b39622e8aca40414c8c668580e94d34b78
Riiturii <riiturii@gmail.com>
2024-05-08 00:27:00 +0200

    ALL OK

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,12 @@
 import Gallery from '../../components/Gallery/Gallery.jsx'
 import HomeBanner from '../../components/Homebanner/Homebanner.jsx'
+import './Home.scss'
 
 function Home() {
     return (
         <main className='home__body'>
-            <div>
             <HomeBanner />
             <Gallery />
-            </div>
         </main>
     )
 }
